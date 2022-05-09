d45dcb7245a2db7fce2166de20e3989065b32787
Irina <hallo.ween@mail.ru>
2022-05-09 13:58:50 +0300

    ui update

--- src/components/pages/main-page/main-page.jsx
+++ src/components/pages/main-page/main-page.jsx
@@ -1,6 +1,6 @@
 import NewsList from '../../news-list/news-list';
 import Button from '../../ui/button/button';
-import styles from  './main-page.module.scss';
+import styles from './main-page.module.scss';
 import { useEffect, useCallback, useState } from 'react';
 import { getNewsThunk } from '../../../store/news/actions';
 import { useDispatch, useSelector, shallowEqual } from 'react-redux';
@@ -34,6 +34,9 @@ const MainPage = () => {
 
     return (
         <section className={styles.mainPage}>
+
+            <h1 className={styles.title}>Spaceflight News:</h1>
+
             <Button filtered={filterNews} onClick={filterNewsToggle}>Filter by like</Button>
             <NewsList newsList={newsList} filterNews={filterNews} />
             {(newsLoading === FETCH_STATUSES.REQUEST) && <Spinner />}
